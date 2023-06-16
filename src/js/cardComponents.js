5304323b2ff8a7d02d9ed1048d18514c2d3c2afe
Fabien <cacrfabfr@gmail.com>
2023-06-16 10:54:02 +0200

    [*] - refactor mentor

--- src/js/cardComponents.js
+++ src/js/cardComponents.js
@@ -1,68 +1,61 @@
-import logoTime from "../assets/time.svg"
+import logoTime from '../assets/time.svg'
 
 class CardComponents extends HTMLElement {
-    constructor() {
-        super()
-        this._time = this.time
-        this._title = this.title
-        this._ingredients = this.ingredients
-        this._description = this.description
-        this.ingredientsLiArray = []
+  constructor() {
+    super()
+    this._time = this.time
+    this._title = this.title
+    this._ingredients = this.ingredients
+    this._description = this.description
+    this.ingredientsLiArray = []
 
-        this.init()
-    }
+    this.init()
+  }
 
-    createCardHTML(headerTitle, recipeTime, ingredients, description) {
-        return `
-          <article class="card">
-            <div class="card__image-container">
+  createCardHTML(headerTitle, recipeTime, ingredients, description) {
+    return `
+          <article class='card'>
+            <div class='card__image-container'>
             </div>
         
-            <div class="card__content-container">
-              <div class="card__header">
-                <h2 class="card__header__title">${headerTitle}</h2>
+            <div class='card__content-container'>
+              <div class='card__header'>
+                <h2 class='card__header__title'>${headerTitle}</h2>
         
-                <div class="recipe__time">
-                  <img src="${logoTime}" alt="cooking time" class="time-logo">
+                <div class='recipe__time'>
+                  <img src='${logoTime}' alt='cooking time' class='time-logo'>
                   <p>${recipeTime}</p>
                 </div>
               </div>
         
-              <div class="card__content">
-                <div class="ingredients__container">
+              <div class='card__content'>
+                <div class='ingredients__container'>
                   <ul>
                     ${ingredients.map(ingredient => ingredient).join('')}
                   </ul>
                   </div>
         
-                  <div class="description__container">
-                    <p class="line-clamp">${description}</p>
+                  <div class='description__container'>
+                    <p class='line-clamp'>${description}</p>
                   </div>
               </div>
             </div>
           </article>
         `
-    }
+  }
 
-    createIngredientsList(ingredient) {
-        this.ingredientsLi = document.createElement('li')
+  createIngredientsList(ingredient) {
+    const { quantity, unit, ingredient: ingre } = ingredient
+    const qty = `${quantity ? quantity : ''} ${unit ? unit : ''}`.trim()
+    const ingredientsLi = `<li><strong>${ingre} ${qty ? ":" : ""} </strong> ${qty}</li>`
+    this.ingredientsLiArray.push(ingredientsLi)
+  }
 
-        if (ingredient.unit === undefined && ingredient.quantity === undefined) {
-            this.ingredientsLi.innerHTML = `<li><strong>${ingredient.ingredient}</strong></li>`
-        } else {
-            if (ingredient.unit === undefined) ingredient.unit = ''
-            if (ingredient.quantity === undefined) ingredient.quantity = ''
-            this.ingredientsLi.innerHTML = `<li><strong>${ingredient.ingredient}:</strong> ${ingredient.quantity} ${ingredient.unit}</li>`
-        }
-
-        this.ingredientsLiArray.push(this.ingredientsLi.innerHTML)
-    }
-
-    init() {
-        // create li element for each ingredient and push it into an array than display it in the card
-        this._ingredients.forEach(ingredient => this.createIngredientsList(ingredient))
-        this.innerHTML = this.createCardHTML(this._title, this._time, this.ingredientsLiArray, this._description)
-    }
+  init() {
+    // create li element for each ingredient and push it into an array than display it in the card
+    this._ingredients.forEach(ingredient => this.createIngredientsList(ingredient))
+    this.innerHTML = this.createCardHTML(this._title, this._time, this.ingredientsLiArray, this._description)
+  }
 }
 
 customElements.get('card-component') || customElements.define('card-component', CardComponents)
